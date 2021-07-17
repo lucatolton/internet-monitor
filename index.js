ba475147ba3f6a115ecec1fbc8ae610422edc2fc
lucatolton <lucatolton@users.noreply.github.com>
2021-07-17 20:30:01 +0100

    change ping amt and add force

--- index.js
+++ index.js
@@ -184,6 +184,14 @@ app.get('/api/speedtest', function(req, res) {
 	});
 });
 
+app.get('/api/force/speedtest', function(req, res) {
+	speedTestFn();
+});
+
+app.get('/api/force/ping', function(req, res) {
+	pingFn();
+});
+
 app.get('/api/config', function(req, res) {
 	var publicConfig = {
 		speedTestEvery: config.speedTestEvery,
